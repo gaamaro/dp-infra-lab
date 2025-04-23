ebb33c1ea54fefc642ce4932163be7f9ddd6829d
Gabriel Amaro <amaro.gabriel@live.com>
2025-04-23 22:20:33 +0000

    Atualiza o bucket do backend do Terraform de "dp-homelab-tfstate" para "dp-demo-tfstate" no arquivo backend.tf.

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "dp-homelab-tfstate"
+    bucket         = "dp-demo-tfstate"
     key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
