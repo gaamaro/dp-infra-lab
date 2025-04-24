2eed0a01895119a0dc2d627a9094bcefabc5583e
Gabriel Amaro <amaro.gabriel@live.com>
2025-04-24 07:40:43 +0000

    backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "dp-lab-tfstate"
+    bucket         = "dp-lab1-tfstate"
     key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
