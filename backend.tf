093ca2b4e56ec996e7c851e078c445bf04fbf235
Gabriel Amaro <amaro.gabriel@live.com>
2025-04-24 15:18:18 +0000

    Atualiza a configuração do backend do Terraform para utilizar o bucket S3 'dp-lab3-tfstate', garantindo a correta persistência do estado da infraestrutura.

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "dp-lab2-tfstate"
+    bucket         = "dp-lab3-tfstate"
     key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
